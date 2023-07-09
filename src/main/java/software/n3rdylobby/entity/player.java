e71b957ca1d8f14806ee426b6e667b728c4cdb7f
Iago "N3rdy" Fragnan <n3rdydzn@hotmail.com>
2023-07-09 11:48:42 -0300

    Delete src/main directory

--- src/main/java/software/n3rdylobby/entity/player.java
+++
@@ -1,8 +0,0 @@
-package software.n3rdylobby.entity;
-
-import java.util.HashMap;
-import java.util.UUID;
-
-public class player {
-    public static HashMap<UUID, Boolean> can_build = new HashMap();
-}
